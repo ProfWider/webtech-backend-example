0036240b83b45148978ff78bf201e68d7c0d532f
Prof. Dr. Arif Wider <wider@htw-berlin.de>
2022-04-06 14:07:01 +0200

    generated spring project

---
+++ src/main/java/de/htw/webtechdemo/WebtechDemoApplication.java
@@ -0,0 +1,13 @@
+package de.htw.webtechdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw/webtechdemo/WebtechDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.webtechdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
